cf8eb7062412f21e2bddc18d7f0df0213a2fe2d7
azankiew <checcol98@hotmail.it>
2020-04-07 17:32:01 +0200

    modificato css tabella

---
+++ CV19/src/cv19/Recensione.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cv19;
+
+/**
+ *
+ * @author checc
+ */
+public class Recensione {
+    
+    private String nome;
+    private String struttura;
+    private String data;
+
+    public Recensione(String nome, String struttura, String data) {
+        this.nome = nome;
+        this.struttura = struttura;
+        this.data = data;
+    }
+    
+    
+    
+}
---
+++ CV19/src/cv19/RecensioniView.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cv19;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author checc
+ */
+public class RecensioniView {
+    
+    @FXML
+    private TableView RecensioniTableView;
+    
+    @FXML
+    private TableColumn nomeTable;
+    
+    @FXML
+    private TableColumn strutturaTable;
+    
+    @FXML
+    private TableColumn dataTable;
+    
+    public void inizialize(){
+        //nomeTable.setCellValueFactory();
+    }
+    
+}
