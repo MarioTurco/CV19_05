b76e6de3182e38401e7d5f78216bb40153525175
Gibser <rauso.giuseppe98@gmail.com>
2020-04-07 23:25:08 +0200

    Merge

--- CV19/src/cv19/LoginController.java
+++ CV19/src/cv19/LoginController.java
@@ -1,5 +1,6 @@
 package cv19;
 
+import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -15,6 +16,10 @@ public class LoginController extends Application {
 
     
     private Stage stage;
+    private Scene mainMenuScene;
+    
+    @FXML
+    private JFXButton loginButton;
     
     @FXML
     private BorderPane loginPane;
@@ -24,8 +29,9 @@ public class LoginController extends Application {
         try {
             this.stage = primaryStage;
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("Login.fxml"));
+            loader.setLocation(getClass().getResource("SideMenu.fxml"));
             loginPane = loader.load();
+            
             Scene scene = new Scene(loginPane);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -40,24 +46,38 @@ public class LoginController extends Application {
             e.printStackTrace();
         }
     }
+    
+    public void launchSideMenu(){
+        try{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("SideMenu.fxml"));
+        BorderPane mainMenuPane = loader.load();
+        mainMenuScene = new Scene(mainMenuPane, 1160, 638);
+        stage.setScene(mainMenuScene);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
     
+    /*
     @FXML
     private void clickLogin(MouseEvent event){
         try{
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("SideMenu.fxml"));
             BorderPane mainMenuPane = loader.load();
-            stage.setWidth(1160);
-            stage.setHeight(638);
-            loginPane.setCenter(mainMenuPane);
+            mainMenuScene = new Scene(mainMenuPane, 1160, 638);
+            this.stage.setScene(mainMenuScene);
             //this.stage.setScene(new Scene(mainMenuPane));
         }
         catch(IOException e){
             e.printStackTrace();
         }
     }
+*/
 }
