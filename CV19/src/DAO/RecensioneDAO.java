46946ae9772074b238cd628823f2af4f03d8ffaa
azankiew <checcol98@hotmail.it>
2020-04-11 19:11:31 +0200

    creato RecensioneDao

--- CV19/src/DAO/RecensioneDAO.java
+++ CV19/src/DAO/RecensioneDAO.java
@@ -5,10 +5,30 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author checc
  */
 public class RecensioneDAO {
+    
+    private final String url = "jdbc:postgresql://database-1.cn8hhgibnvsj.eu-central-1.rds.amazonaws.com:5432/database-1";
+    private final String user = "cercaviaggi";
+    private final String password = "cercaviaggi";
+    
+    public Connection getConnection(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
     
 }
