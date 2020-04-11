37a6cc54bd33f5e204b91dc956add15c83b22c1d
TurkishKebab <mario.turco4@studenti.unina.it>
2020-04-11 19:32:52 +0200

    Modifica dei model

--- CV19/src/presenter/RecensioniController.java
+++ CV19/src/presenter/RecensioniController.java
@@ -5,17 +5,23 @@
  */
 package presenter;
 
+import java.io.IOException;
 import model.Recensione;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-
+import javafx.beans.value.ChangeListener;
 /**
  *
  * @author checc
@@ -48,6 +54,16 @@ public class RecensioniController implements Initializable{
         dataTable.setCellValueFactory(new PropertyValueFactory<Recensione,String>("data"));
         
         RecensioniTableView.setItems(data);
-    }
+}
     
+    private void viewRecensione(){
+       Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/VisualizzaRecensione.fxml"));
+
+        } catch (IOException ex) {
+            Logger.getLogger(SideMenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       // borderpane.setCenter(root);
+    }
 }
