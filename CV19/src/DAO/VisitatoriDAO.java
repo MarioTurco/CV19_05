5480a5ebc448eae5017a8cc897f89e8986773b55
TurkishKebab <mario.turco4@studenti.unina.it>
2020-04-11 21:05:27 +0200

    Modificato metodo getVisitatori

--- CV19/src/DAO/VisitatoriDAO.java
+++ CV19/src/DAO/VisitatoriDAO.java
@@ -11,6 +11,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -35,14 +36,29 @@ public class VisitatoriDAO {
         }
         return conn;
     }
-
+    public ArrayList<Visitatore> getAllVisitatori(){
+        PreparedStatement statement = prepareQuery();
+        ResultSet resultSet = executeStatement(statement);
+        ArrayList<Visitatore> visitatori =  getVisitatoriFromResultSet(resultSet);
+        return visitatori;
+    }
+    
     public Visitatore getVisitatoreByNickname(String nickname) {
         PreparedStatement statement = prepareQuerySelectByNickname(nickname);
         ResultSet resultSet = executeStatement(statement);
-        Visitatore visitatore = getVisitatoreFromResultSet(resultSet);
-        return visitatore;
+        ArrayList<Visitatore> visitatori = getVisitatoriFromResultSet(resultSet);
+        return visitatori.get(0);
     }
-
+    private PreparedStatement prepareQuery(){
+        PreparedStatement statement = null;
+        String query = "SELECT * FROM public.visitatori ";
+        try {
+            statement = getConnection().prepareStatement(query);
+        } catch (SQLException sqlException) {
+            //TODO implementare qualcosa qui
+        }
+        return statement;
+    }
     private PreparedStatement prepareQuerySelectByNickname(String nickname) {
         PreparedStatement statement = null;
         String query = "SELECT * FROM public.visitatori WHERE nickname = ?";
@@ -54,9 +70,8 @@ public class VisitatoriDAO {
         }
         return statement;
     }
-
-    private Visitatore getVisitatoreFromResultSet(ResultSet resultSet) {
-        Visitatore visitatore = null;
+    private ArrayList<Visitatore> getVisitatoriFromResultSet(ResultSet resultSet) {
+        ArrayList<Visitatore> visitatori = new <Visitatore>ArrayList();
         String nome = null, email = null, nickname = null, dataDiNascita = null;
         int recensioniApprovate = 0, recensioniRifiutate = 0;
         try {
@@ -67,13 +82,14 @@ public class VisitatoriDAO {
                 dataDiNascita = resultSet.getString("dataDiNascita");
                 recensioniApprovate = resultSet.getInt("recensioniApprovate");
                 recensioniRifiutate = resultSet.getInt("recensioniRifiutate");
+                visitatori.add( new Visitatore(nome, nickname, email, dataDiNascita, recensioniApprovate, recensioniRifiutate) );
             }
         } catch (SQLException sqlException) {
             //TODO implementare qualcosa
         }
-        return new Visitatore(nome, nickname, email, dataDiNascita, recensioniApprovate, recensioniRifiutate);
+        return visitatori;
     }
-
+    
     private ResultSet executeStatement(PreparedStatement statement) {
         ResultSet resultSet = null;
         try {
