ee1339d4f609c11bbf1d28e7f8cb197d1632ba9b
Gibser <rauso.giuseppe98@gmail.com>
2020-04-11 20:37:56 +0200

    Funzionalità login amministratore

--- CV19/src/DAO/AdminDAO.java
+++ CV19/src/DAO/AdminDAO.java
@@ -5,10 +5,77 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author gpepp
  */
 public class AdminDAO {
+    private final String url = "jdbc:postgresql://database-1.cn8hhgibnvsj.eu-central-1.rds.amazonaws.com:5432/postgres";
+    private final String user = "admin_cv19";
+    private final String password = "cvuser";
     
+    //private Connection dbConnection;
+    
+    /*
+    public AdminDAO(){
+        try {
+            dbConnection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }*/
+    
+    public Connection getConnection(){
+        Connection dbConnection = null;
+        try {
+            dbConnection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return dbConnection;
+    }
+    
+    private boolean emptyResultSet(ResultSet rs) throws SQLException{
+        return rs.next() == false;
+    }
+    
+    
+    public boolean tryLogin(String username, String password){
+        Connection dbConnection = getConnection();
+        String loginQuery = "SELECT * FROM ADMINISTRATOR WHERE USERNAME = ? AND PASSWORD = ?";
+        ResultSet rs = null;
+        boolean result = false;
+        
+        try{
+            PreparedStatement loginPreparedStatement = dbConnection.prepareStatement(loginQuery);
+            loginPreparedStatement.setString(1, username);
+            loginPreparedStatement.setString(2, password);
+            
+            rs = loginPreparedStatement.executeQuery();
+            
+            result = !emptyResultSet(rs);
+            System.out.println(!result);
+            
+            loginPreparedStatement.close();
+            rs.close();
+            dbConnection.close();
+           
+        }
+        catch(NullPointerException np){
+            np.printStackTrace();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        } 
+        return result;
+    }
 }
--- CV19/src/presenter/LoginController.java
+++ CV19/src/presenter/LoginController.java
@@ -15,56 +15,143 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import DAO.AdminDAO;
+import java.util.Optional;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.stage.Screen;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author gpepp
  */
 public class LoginController {
-    
+
     @FXML
-    public void clickLogin(ActionEvent event) throws IOException{
-        Parent homePageParent = FXMLLoader.load(getClass().getResource("/view/SideMenu.fxml"));
-        Scene homePageScene = new Scene(homePageParent);
-        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    private AdminDAO administratorDao;
+
+    
+    
+    public LoginController() {
+        initAdminDAO();
+    }
+
+    private void initAdminDAO() {
+        administratorDao = new AdminDAO();
+    }
+
+    
+    
+    
+    private Parent getParent() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/view/SideMenu.fxml"));
+    }
+
+    private Scene getScene(Parent root) {
+        return new Scene(root);
+    }
+
+    private void showStage(Stage appStage, Scene sceneToSet) {
         appStage.hide(); //optional
-        appStage.setScene(homePageScene);
+        appStage.setScene(sceneToSet);
         appStage.show();
+    }
+
+    private void loadSideMenuPanelAfterLogin(ActionEvent event) throws IOException {
+        Parent homePageParent = getParent();
+        Scene homePageScene = getScene(homePageParent);
+
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        showStage(appStage, homePageScene);
+
         loadRecensioniView(homePageScene);
         initRecensioniButton(homePageScene);
     }
-    
-    private void loadRecensioniView(Scene homePageScene){
-        Parent root = null;
-        BorderPane borderpane = (BorderPane)homePageScene.lookup("#borderpane");
+
+    private boolean tryLogin() {
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+
+        return administratorDao.tryLogin(username, password);
+    }
+
+    private void showLoginErrorDialog() {
+        Alert dg = new Alert(Alert.AlertType.INFORMATION);
+        dg.setHeaderText("Errore Login");
+        dg.setContentText("Credenziali errate.");
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        dg.setX((bounds.getMaxX() / 2) - 150);
+        dg.setY((bounds.getMaxY() / 2) - 100);
+
+        DialogPane dialogPane = dg.getDialogPane();
+        dialogPane.getStylesheets().add(
+                getClass().getResource("/css/dialogStyle.css").toExternalForm());
+        dg.initStyle(StageStyle.UNDECORATED);
+        Image icon = new Image("/icons/infoIcon.png");
+        ImageView iv = new ImageView(icon);
+        dg.getDialogPane().setGraphic(iv);
+
+        Optional<ButtonType> result = dg.showAndWait();
+        //ButtonType button = result.orElse(ButtonType.CANCEL);
+    }
+
+    @FXML
+    public void clickLogin(ActionEvent event) {
+
         try {
-           root = FXMLLoader.load(getClass().getResource("/view/Recensioni.fxml"));
+            if (tryLogin()) {
+                loadSideMenuPanelAfterLogin(event);
+            } else {
+                showLoginErrorDialog();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void loadRecensioniView(Scene homePageScene) {
+        Parent root = null;
+        BorderPane borderpane = (BorderPane) homePageScene.lookup("#borderpane");
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/Recensioni.fxml"));
         } catch (IOException ex) {
             Logger.getLogger(SideMenuController.class.getName()).log(Level.SEVERE, null, ex);
         }
         borderpane.setCenter(root);
     }
-    
-    private void initRecensioniButton(Scene homePageScene){
-        JFXButton recensioniButton = (JFXButton)homePageScene.lookup("#recensioniButton");
-        ImageView recensioniImageView = (ImageView)homePageScene.lookup("#recensioniImageView");
-        
+
+    private void initRecensioniButton(Scene homePageScene) {
+        JFXButton recensioniButton = (JFXButton) homePageScene.lookup("#recensioniButton");
+        ImageView recensioniImageView = (ImageView) homePageScene.lookup("#recensioniImageView");
+
         setBlueIcon(recensioniImageView);
-        
+
         changeRecensioniButtonTextColourToBlue(recensioniButton);
-        
+
     }
-    
-    private void setBlueIcon(ImageView recensioniImageView){
+
+    private void setBlueIcon(ImageView recensioniImageView) {
         Image image = new Image(getClass().getResourceAsStream("/icons/recensioni-blue.png"));
         recensioniImageView.setImage(image);
     }
-    
-    private void changeRecensioniButtonTextColourToBlue(JFXButton recensioniButton){
+
+    private void changeRecensioniButtonTextColourToBlue(JFXButton recensioniButton) {
         recensioniButton.setStyle("-fx-text-fill: #3282B8");
     }
 }
