9b84f4db0ec0e27d4f0414ac23a0c3b961e3ff5a
TurkishKebab <mario.turco4@studenti.unina.it>
2020-04-21 19:41:07 +0200

    Aggiunte varie cose

---
+++ provaCV/app/src/main/java/com/example/provacv/FiltriFragment.java
@@ -0,0 +1,57 @@
+package com.example.provacv;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.mapbox.mapboxsdk.maps.SupportMapFragment;
+
+
+public class FiltriFragment extends Fragment {
+    private ImageButton backButton;
+    private ViewGroup container;
+    private SupportMapFragment mapFragment;
+
+    public FiltriFragment(SupportMapFragment mapFragment) {
+        this.mapFragment = mapFragment;
+    }
+
+
+    public static FiltriFragment newInstance(SupportMapFragment mapFragment) {
+        FiltriFragment fragment = new FiltriFragment(mapFragment);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        this.container = container;
+        return inflater.inflate(R.layout.fragment_filtri, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        backButton = view.findViewById(R.id.backButtonFiltri);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).toolbar.setVisibility(View.VISIBLE);
+                ((MainActivity)getActivity()).setMap(savedInstanceState);
+            }
+        });
+    }
+}
--- provaCV/app/src/main/java/com/example/provacv/MainActivity.java
+++ provaCV/app/src/main/java/com/example/provacv/MainActivity.java
@@ -8,8 +8,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
-import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -23,7 +24,7 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
+    ImageButton filtriButton;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     NavigationView navigationView;
@@ -34,38 +35,60 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolbar);
         navigationView = findViewById(R.id.navigationView);
         navigationView.getBackground().setAlpha(122);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
         setMap(savedInstanceState);
+        filtriButton = findViewById(R.id.filtriButton);
+        filtriButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFiltriFragment();
+            }
+        });
+    }
 
+    private void loadFiltriFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, FiltriFragment.newInstance(mapFragment) , "filtriFragment" );
+        transaction.commit();
+        toolbar.setVisibility(View.GONE);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawerLayout.closeDrawers();
         switch(menuItem.getItemId()){
             case R.id.login:
+                loadLoginFragment();
                 Toast.makeText(MainActivity.this, "Login selezionato", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.signup:
+                loadSignupFragment();
                 Toast.makeText(MainActivity.this, "Registrati selezionato", Toast.LENGTH_SHORT).show();
                 break;
         }
         return false;
     }
 
-    private void setMap(Bundle savedInstanceState){
-        Mapbox.getInstance(this,"pk.eyJ1IjoibWFyaW90dXJjbzQiLCJhIjoiY2s5NXZicG8zMG81aDNsbzFudmJtbXFvZCJ9.SAKPHTJnSi4BpAcRkBRclA");
+    private void loadSignupFragment() {
+    }
 
+    private void loadLoginFragment() {
+       
+    }
+
+
+    protected void setMap(Bundle savedInstanceState){
+        Mapbox.getInstance(this,"pk.eyJ1IjoibWFyaW90dXJjbzQiLCJhIjoiY2s5NXZicG8zMG81aDNsbzFudmJtbXFvZCJ9.SAKPHTJnSi4BpAcRkBRclA");
         if (savedInstanceState == null) {
             // Create fragment
             final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -88,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             mapFragment.getMapAsync(new OnMapReadyCallback() {
                 @Override
                 public void onMapReady(@NonNull MapboxMap mapboxMap) {
-                    mapboxMap.setStyle(Style.SATELLITE, new Style.OnStyleLoaded() {
+                    mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                         @Override
                         public void onStyleLoaded(@NonNull Style style) {
 
@@ -101,6 +124,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             });
         }
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
 }
