d1b556bb2baf683364075b6faa6428f8d4e6d598
TurkishKebab <mario.turco4@studenti.unina.it>
2020-04-21 12:23:29 +0200

    modifiche layout

---
+++ CercaViaggi/app/src/main/java/com/example/cercaviaggi19/activityProva.java
@@ -0,0 +1,14 @@
+package com.example.cercaviaggi19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityProva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prova);
+    }
+}
