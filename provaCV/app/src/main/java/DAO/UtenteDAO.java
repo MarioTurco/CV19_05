53f31fe3fd0ead80ab95255cb70541e62e2abf84
azankiew <checcol98@hotmail.it>
2020-04-25 21:00:46 +0200

    prova

--- provaCV/app/src/main/java/DAO/UtenteDAO.java
+++ provaCV/app/src/main/java/DAO/UtenteDAO.java
@@ -16,6 +16,21 @@ import org.json.JSONObject;
 
 public class UtenteDAO {
 
+    private class BooleanContainer{
+        private boolean val;
+        protected BooleanContainer(boolean val){
+            this.val=val;
+        }
+
+        protected boolean getVal(){
+            return val;
+        }
+
+        protected void setVal(boolean val){
+            this.val=val;
+        }
+    }
+
     private Context context;
 
     public UtenteDAO(Context context){
@@ -38,15 +53,15 @@ public class UtenteDAO {
         String queryRequestString = "https://m6o9t2bfx0.execute-api.eu-central-1.amazonaws.com/test3/table?table=utente";
         queryRequestString +=appendRequestForLogin(username);
         System.out.println(queryRequestString);
-        final boolean [] loginResult = new boolean[1];
+        final BooleanContainer booleanContainer = new BooleanContainer(false);
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
                 (Request.Method.GET, queryRequestString, null, new Response.Listener<JSONArray>() {
 
                     @Override
                     public void onResponse(JSONArray response) {
                         try {
-                            loginResult[0] = checkPassword(response.getJSONObject(0), givenPassword);
-                            System.out.println(loginResult[0]);
+                            booleanContainer.setVal(checkPassword(response.getJSONObject(0), givenPassword));
+                            System.out.println(booleanContainer.getVal());
                         } catch (JSONException e) {}
                     }
                 }, new Response.ErrorListener() {
@@ -58,7 +73,7 @@ public class UtenteDAO {
                     }
                 });
         queue.add(jsonArrayRequest);
-        System.out.println("valora di ritorno"+ loginResult[0]);
-        return loginResult[0];
+        System.out.println("valora di ritorno "+ booleanContainer.getVal());
+        return booleanContainer.getVal();
     }
 }
