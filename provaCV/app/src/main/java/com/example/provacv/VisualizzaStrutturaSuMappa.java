418c5ec6b475e17ec2d73e49aca7286d9d759f5c
TurkishKebab <mario.turco4@studenti.unina.it>
2020-04-29 18:26:48 +0200

    m

--- provaCV/app/src/main/java/com/example/provacv/VisualizzaStrutturaSuMappa.java
+++ provaCV/app/src/main/java/com/example/provacv/VisualizzaStrutturaSuMappa.java
@@ -28,21 +28,9 @@ public class VisualizzaStrutturaSuMappa extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VisualizzaStrutturaSuMappa.
-     */
     // TODO: Rename and change types and number of parameters
-    public static VisualizzaStrutturaSuMappa newInstance(String param1, String param2) {
+    public static VisualizzaStrutturaSuMappa newInstance() {
         VisualizzaStrutturaSuMappa fragment = new VisualizzaStrutturaSuMappa();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
