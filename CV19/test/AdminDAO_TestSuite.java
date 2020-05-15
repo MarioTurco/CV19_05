1878d3bf259c6dcc85772d1ba2e2bd5fae84ab9c
TurkishKebab <mario.turco4@studenti.unina.it>
2020-05-15 11:18:44 +0200

    Aggiunto testing sect black box del metodo try login

--- CV19/test/AdminDAO_TestSuite.java
+++ CV19/test/AdminDAO_TestSuite.java
@@ -38,31 +38,95 @@ public class AdminDAO_TestSuite {
     public void tearDown() {
     }
     
-    //Black-box
+    //Black-box SECT
+    //1-5
     @Test
-    public void tryLogin_passwordErrata_test(){
-        assertEquals( dao.tryLogin("admin", null), false);
-    }
-    @Test
-    public void tryLogin_usernameErrato_test(){
-        assertEquals( dao.tryLogin(null, "admin"), false);
-    }
-     @Test
-    public void tryLogin_credenzialiCorrette_test(){
-        assertEquals( dao.tryLogin("admin", "admin"), true);
-    } 
-    
-    @Test
-    public void tryLogin_EntrambeCredenzialiErrate_test(){
+    public void tryLogin_EntrambeCredenzialiNull_test(){
         assertEquals( dao.tryLogin(null, null), false);
     }
     
+    //1-6
     @Test
-    public void tryLogin_passwordVuota_test(){
+    public void tryLogin_usernameNull_passwordVuota_test(){
+        assertEquals( dao.tryLogin(null, ""), false);
+    }
+    
+    //1-7
+    @Test
+    public void tryLogin_usernameNull_passwordErrata_test(){
+        assertEquals( dao.tryLogin(null, "sbagliata"), false);
+    }
+    //1-8
+    @Test
+    public void tryLogin_usernameNull_passwordCorretta_test(){
+        assertEquals( dao.tryLogin(null, "admin"), false);
+    }
+    
+    //2-5
+    @Test
+    public void tryLogin_usernameVuoto_passwordNull_test(){
+        assertEquals( dao.tryLogin("", null), false);
+    }
+    
+    //2-6
+    @Test
+    public void tryLogin_usernameVuoto_passwordVuota_test(){
+        assertEquals( dao.tryLogin("", ""), false);
+    }
+    
+    //2-7
+    @Test
+    public void tryLogin_usernameVuoto_passwordErrata_test(){
+        assertEquals( dao.tryLogin("admin", "sbagliata"), false);
+    }
+    //2-8
+    @Test
+    public void tryLogin_usernameVuoto_passwordCorretta_test(){
+        assertEquals( dao.tryLogin(null, "admin"), false);
+    }
+    //3-5
+    @Test
+    public void tryLogin_usernameSbagliato_passwordNull_test(){
+        assertEquals( dao.tryLogin("sbagliato!!!", null), false);
+    }
+    
+    //3-6
+    @Test
+    public void tryLogin_usernameSbagliato_passwordVuota_test(){
+        assertEquals( dao.tryLogin("sbagliato!!!", ""), false);
+    }
+    
+    //3-7
+    @Test
+    public void tryLogin_usernameSbagliato_passwordErrata_test(){
+        assertEquals( dao.tryLogin("sbagliato!!!", "sbagliata!"), false);
+    }
+    //3-8
+    @Test
+    public void tryLogin_usernameSbagliato_passwordCorretta_test(){
+        assertEquals( dao.tryLogin("sbagliato!!!", "admin"), false);
+    }
+    //4-5
+    @Test
+    public void tryLogin_usernameCorretto_passwordNull_test(){
+        assertEquals( dao.tryLogin("admin", null), false);
+    }
+    
+    //4-6
+    @Test
+    public void tryLogin_usernameCorretto_passwordVuota_test(){
         assertEquals( dao.tryLogin("admin", ""), false);
     }
-     @Test
-    public void tryLogin_usernameVuoto_test(){
-        assertEquals( dao.tryLogin("", "admin"), false);
+    
+    //4-7
+    @Test
+    public void tryLogin_usernameCorretto_passwordErrata_test(){
+        assertEquals( dao.tryLogin("admin", "sbagliata!"), false);
     }
+    //4-8
+    @Test
+    public void tryLogin_usernameCorretto_passwordCorretta_test(){
+        assertEquals( dao.tryLogin("admin", "admin"), true);
+    }
+    
 }
