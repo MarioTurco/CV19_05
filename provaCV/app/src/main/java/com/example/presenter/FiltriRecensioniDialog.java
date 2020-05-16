76806e41b956bc91098d5972aeba53db2c9a6c79
Gibser <rauso.giuseppe98@gmail.com>
2020-05-16 15:18:39 +0200

    Dialog filtri

--- provaCV/app/src/main/java/com/example/presenter/FiltriRecensioniDialog.java
+++ provaCV/app/src/main/java/com/example/presenter/FiltriRecensioniDialog.java
@@ -1,6 +1,7 @@
 package com.example.presenter;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,5 +18,17 @@ public class FiltriRecensioniDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.filtri_recensioni_dialog, null, false);
         return view;
+    }
+
+    private void setDimensioni(){
+        int height = 1100;
+        int width = 1000;
+        getDialog().getWindow().setLayout(width, height);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setDimensioni();
     }
 }
