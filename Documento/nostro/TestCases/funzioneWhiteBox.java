f208094534981d0f09d4b04dc4ba35862fc1c3a0
TurkishKebab <mario.turco4@studenti.unina.it>
2020-05-18 22:47:39 +0200

    Aggiornato documento

---
+++ Documento/nostro/TestCases/funzioneWhiteBox.java
@@ -0,0 +1,22 @@
+public String getFiltriNonVuoti(double longitudine, double latitudine) {
+    String returnString = "";
+    if (!nome.equals(""))
+        returnString += "&nome=%25" + nome + "%25";
+    if (!citta.equals(""))
+        returnString += "&citta=%25" + citta + "%25";
+    if (!categoria.equals("Nessuno"))
+        returnString += "&categoria=" + categoria;
+    if (!prezzo.equals("Nessuno"))
+        returnString += "&prezzo=" + prezzo;
+    if (!valutazione.equals("Nessuno"))
+        returnString += "&valutazione_media=" + nome;
+    if (prossimita) {
+        if (!distanzaMassima.equals(""))
+            returnString += "&radius=" + distanzaMassima;
+        else
+            returnString += "&radius=0";
+        returnString += "&x=" + longitudine;
+        returnString += "&y=" + latitudine;
+    }
+    return returnString;
+}
